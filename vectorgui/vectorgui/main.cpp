278ba46610e882efbf03c184a0ab7e053da1153d
kdyung <kyung8888@gmail.com>
2019-11-04 13:30:16 -0800

    set up OpenGL libraries

---
+++ vectorgui/vectorgui/main.cpp
@@ -0,0 +1,10 @@
+//Tutorial for setting up libraries: https://learnopengl.com/Getting-started/Creating-a-window
+//https://www.youtube.com/watch?v=k9LDF016_1A
+
+#include <glad/glad.h> 
+#include <GLFW\glfw3.h>
+
+int main() 
+{
+	return 0;
+}
